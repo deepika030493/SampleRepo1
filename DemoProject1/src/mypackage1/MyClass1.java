aef1fb582add16f3693bec4e116bf9d19f693e3e
deepika030493 <123857598+deepika030493@users.noreply.github.com>
2023-03-30 17:46:39 +0530

    BasicTask1

---
+++ DemoProject1/src/mypackage1/MyClass1.java
@@ -0,0 +1,13 @@
+package mypackage1;
+
+public class MyClass1 {
+
+	public static void main(String[]args) {
+
+		System.out.println("TestBase");
+		System.out.println("Pom.xml");
+		System.out.println("Utilities");
+		System.out.println("Resources");
+		System.out.println("Suite");
+	}
+	}
